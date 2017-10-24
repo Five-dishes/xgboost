93d8d9e3dda4253b249ae2bde4fbcbe4d9dbd886
ChangZihao <648639425@qq.com>
2017-10-24 20:51:28 +0800

    second commit

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
 # -*- coding: UTF-8 -*-
-
 """
-Count # of unique orders for each mid-class item per day.
 Input file: data.csv;
 Headers in data.csv: custid	大类编码	中类编码	销售日期	销售数量	是否促销;
 Other columns has been deleted manually.
-Output file: processed_1.csv
-Headers in processed_1.csv: 大类编码	中类编码	销售日期	销售数量    	day of week 	day of week 2
-Although last 2 columns will never be used...
- """
+"""
 
 import pandas as pd
 import datetime
 
-DIR_PATH = "/Users/CZH/Downloads/zero/"
-
+DIR_PATH = '/Users/CZH/Downloads/zero/'
 group_indices = ['大类编码', '中类编码', '销售日期']
 
 
@@ -41,15 +35,10 @@ def df_reduce(df: pd.DataFrame) -> pd.DataFrame:
     """
     :param df: pd.DataFrame
     :return: pd.DataFrame
-    This function roll up the data frame by custid and '销售数量';
+    This function roll up the data frame by custid and '销售数量';'中类编码'
     Define an one-line function for type hint
     """
     return df.groupby(group_indices).apply(add_up)
 
 
-df = pd.read_csv(DIR_PATH + 'data.csv', sep=',', header=0, encoding='gbk')
-reduced = df_reduce(df)
-reduced.drop('custid', axis=1, inplace=True)
-reduced = reduced[reduced['是否促销'] == '否']  #delet element with 促销 attribute
-reduced.drop('是否促销', axis=1, inplace=True)
-reduced.to_csv(DIR_PATH + 'processed_1.csv', sep=',')
+
